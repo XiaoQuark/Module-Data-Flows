75eb70dc36e1ceb9b8669ecf11dd6e9e0cc33a22
XiaoQuark <kris.oldrini@gmail.com>
2026-04-20 21:50:19 +0100

    refactor: clear form after submission

--- debugging/book-library/script.js
+++ debugging/book-library/script.js
@@ -41,6 +41,10 @@ function submit() {
   } else {
     let book = new Book(title.value, author.value, pages.value, check.checked);
     myLibrary.push(book);
+    title.value = "";
+    author.value = "";
+    pages.value = "";
+    check.checked = false;
     render();
   }
 }
